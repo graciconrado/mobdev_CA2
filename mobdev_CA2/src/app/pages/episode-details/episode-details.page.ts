d8074b27f190193f76b16cd49a47f7a5961dcf29
graciconrado <graciconrado@gmail.com>
2020-04-25 20:46:37 +0000

    favourite function

--- mobdev_CA2/src/app/pages/episode-details/episode-details.page.ts
+++ mobdev_CA2/src/app/pages/episode-details/episode-details.page.ts
@@ -1,6 +1,8 @@
 import { ApiService } from './../../services/api.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FavouriteService } from '../../services/favourite.service';
+
 @Component({
     selector: 'app-episode-details',
     templateUrl: './episode-details.page.html',
@@ -8,12 +10,31 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EpisodeDetailsPage implements OnInit {
     episode: any;
+    isFavourite = false;
     episodeId = null;
-    constructor(private activatedRoute: ActivatedRoute, private api: ApiService) { }
+
+    constructor(private activatedRoute: ActivatedRoute, private api: ApiService,
+        private FavouriteService: FavouriteService) { }
+
     ngOnInit() {
         this.episodeId = this.activatedRoute.snapshot.paramMap.get('id');
         this.api.getEpisode(this.episodeId).subscribe(res => {
             this.episode = res[0];
-        })
+        });
+
+        this.FavouriteService.isFavourite(this.episodeId).then(isFav => {
+            this.isFavourite = isFav;
+        });
+    }
+
+    favouriteEpisode() {
+        this.FavouriteService.favouriteEpisode(this.episodeId).then(() => {
+            this.isFavourite = true;
+        });
+    }
+    unfavouriteEpisode() {
+        this.FavouriteService.unfavouriteEpisode(this.episodeId).then(() => {
+            this.isFavourite = false;
+        });
     }
 }
--- mobdev_CA2/src/app/services/favourite.service.spec.ts
+++ mobdev_CA2/src/app/services/favourite.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { FavoriteService } from './favorite.service';
+import { FavouriteService } from './favourite.service';
 
-describe('FavoriteService', () => {
+describe('FavouriteService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: FavoriteService = TestBed.get(FavoriteService);
+    const service: FavouriteService = TestBed.get(FavouriteService);
     expect(service).toBeTruthy();
   });
 });
